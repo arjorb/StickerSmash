aeed4a2ae506235b5441c4837aac6eb5444e8ad1
John U <john@John-M.local>
2024-07-06 08:29:01 +0200

    refactor: Add lazy loading for images in App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
+const PlaceholderImage = require("./assets/images/background-image.png");
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#fff'}}>Hello world!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -13,8 +15,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 320,
+    height: 440,
+    paddingTop: 58,
+    
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 18,
   },
 });
